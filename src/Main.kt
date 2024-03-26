1c02053b49b29e7f80f9ef191724b74dbc47ee61
gustavorods <gustavo.rlsilva07@gmail.com>
2024-03-25 22:44:48 -0300

    feat: delegate funcitons in smarrTvDice e SmartLightDevice

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,6 @@
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
 // Superclass
 open class SmartDevice(val name: String, val category: String) {
 
@@ -24,19 +27,8 @@ class SmartTvDevice(deviceName: String, deviceCategory: String) : // Inheriting 
 
     override val deviceType = "Smart TV"
 
-    var speakerVolume = 2
-        set(value) {
-            if (value in 0..100) { // Defining a condition for the variable to be redefined
-                field = value
-            }
-        }
-
-    var channelNumber = 1
-        set(value) {
-            if (value in 0..200) { // Defining a condition for the variable to be redefined
-                field = value
-            }
-        }
+    private var speakerVolume by RangeRegulator(initialValue = 2, minValue = 0, maxValue = 100)
+    private var channelNumber by RangeRegulator(initialValue = 1, minValue = 0, maxValue = 200)
 
     fun increaseSpeakerVolume() {
         speakerVolume++
@@ -68,12 +60,7 @@ class SmartLightDevice(deviceName: String, deviceCategory: String)
 
     override val deviceType = "Smart Light"
 
-    var britghtnessLevel = 0
-            set(value) {
-                if(value in 0..100) {
-                    field = value
-                }
-            }
+    private var britghtnessLevel by RangeRegulator(initialValue = 0, minValue = 0, maxValue = 100)
 
     fun increaseBrightness() {
         britghtnessLevel++
@@ -143,6 +130,27 @@ class SmartHome(val smartTvDevice: SmartTvDevice, val smartLightDevice: SmartLig
         turnOffTv()
         turnOffLight()
     }
+}
+
+// Range Regulator
+class RangeRegulator(
+    initialValue: Int,
+    private val minValue: Int,
+    private val maxValue: Int) : ReadWriteProperty<Any?, Int> {
+
+        var fieldData = initialValue
+    override fun getValue(thisRef: Any?, property: KProperty<*>): Int {
+        return fieldData
+    }
+
+
+    override fun setValue(thisRef: Any?, property: KProperty<*>, value: Int) {
+        if(value in minValue..maxValue) {
+            fieldData = value
+        }
+    }
+
+
 }
 
 // Main
